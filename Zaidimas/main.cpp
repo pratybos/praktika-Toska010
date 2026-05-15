85c47eeee10f5c7d2e455d3dfe038f69a350a09f
Tautvydas Bagdžius <toska.010@gmail.com>
2026-05-15 12:07:03 +0300

    Zaidejo figura

--- Zaidimas/main.cpp
+++ Zaidimas/main.cpp
@@ -1,6 +1,23 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
+float zaidejoGreitis = 100u;
+float zaidejoPlotas = 40u;
+float zaidejoAukstis = 15u;
+struct Zaidejas {
+    sf::ConvexShape figura;
+    Zaidejas() {
+        figura = sf::ConvexShape(3);
+        //Kaire Desine Virsus
+        figura.setPoint(0, {0u, 0u});
+        figura.setPoint(1, {zaidejoPlotas, 0u});
+        figura.setPoint(2, {zaidejoPlotas * 0.5f, zaidejoAukstis });
+        //Kaire Desine Virsus
+        figura.setFillColor(sf::Color::Green);
+        figura.setPosition({0u, 0u});
+    }
+};
+
 int main()
 {
     sf::RenderWindow window(sf::VideoMode({1280u, 720u}), "Zaidimas");
@@ -10,8 +27,11 @@ int main()
                 window.close();
             }
         }
+        Zaidejas zaidejas;
         window.clear();
+        window.draw(zaidejas.figura);
         window.display();
+
     }
     return 0;
 }
