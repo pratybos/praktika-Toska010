10c566077ca4d24ca2823cd3925060f83149485b
Tautvydas Bagdžius <toska.010@gmail.com>
2026-05-15 11:34:19 +0300

    Game loop

--- Zaidimas/main.cpp
+++ Zaidimas/main.cpp
@@ -1,7 +1,17 @@
+#include <SFML/Graphics.hpp>
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    sf::RenderWindow window(sf::VideoMode({1280u, 720u}), "Zaidimas");
+    while (window.isOpen()) {
+        while (const std::optional<sf::Event> e = window.pollEvent()) {
+            if (e->is<sf::Event::Closed>()) {
+                window.close();
+            }
+        }
+        window.clear();
+        window.display();
+    }
     return 0;
 }
